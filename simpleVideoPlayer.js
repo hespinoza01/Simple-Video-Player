c9d50d715ad85eced048675090fb42c95bc029df
Harold Espinoza <haroldesptru@gmail.com>
2019-01-21 12:48:09 -0600

    Añadido el evento Play/Pause, también habilitada la barra de progreso para mostrar el transcurso del video.

--- simpleVideoPlayer.js
+++ simpleVideoPlayer.js
@@ -1,15 +1,34 @@
-const videoPlayer = document.getElementById('simpleVideoPlayer');
-const videoPlayerMedia = videoPlayer.querySelector('.media');
-const btnPlay = videoPlayer.querySelector('.playControl');
+const videoPlayer = document.getElementById('simpleVideoPlayer'),
+      videoPlayerMedia = videoPlayer.querySelector('.media'),
+      btnPlay = videoPlayer.querySelector('.playControl');
+
+let bucleMediaState = undefined;
 
 btnPlay.addEventListener('click', e => {
   let textContent = e.target.textContent;
-console.log(textContent);
+
   if(textContent === "Play"){
     e.target.textContent = "Pause";
     videoPlayerMedia.play();
+    bucleMediaState = setInterval(mediaState, 1000);
   }else if(textContent === "Pause"){
     e.target.textContent = "Play";
     videoPlayerMedia.pause();
+    clearInterval(bucleMediaState);
   }
 });
+
+function mediaState(){
+  let max = 100,
+      progressValue = 0,
+      progressIndicator = videoPlayer.querySelector('.barProgress');
+
+  if(!videoPlayerMedia.ended){
+    progressValue = parseInt(videoPlayerMedia.currentTime * max / videoPlayerMedia.duration);
+    progressIndicator.style.width = `${progressValue}%`;
+  }else{
+    progressIndicator.style.width = '0%';
+    btnPlay.textContent = 'Play';
+    clearInterval(bucleMediaState);
+  }
+}
