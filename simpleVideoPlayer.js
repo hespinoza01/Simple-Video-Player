3e53d3352b2aed55fadca17fae438dbdbdde29fd
Harold Espinoza <haroldesptru@gmail.com>
2019-01-22 20:24:11 -0600

    Optimizada la función del evento de control de volumen. Arreglada y completa la funcionabilidad del video a pantalla completa, también se añadió el icono del mismo.

--- simpleVideoPlayer.js
+++ simpleVideoPlayer.js
@@ -11,8 +11,12 @@ const icons = {
   play: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M7 6v12l10-6z'/></svg>",
   pause: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M8 7h3v10H8zM13 7h3v10h-3z'/></svg>",
   mute: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M13.472 3.118c-.324-.174-.72-.154-1.026.05L7.727 6.313l-4.02-4.02L2.293 3.707l18 18 1.414-1.414-2.02-2.02c1.441-1.686 2.312-3.85 2.312-6.273 0-4.091-2.472-7.453-5.999-9v2c2.387 1.386 3.999 4.047 3.999 7 0 1.832-.63 3.543-1.671 4.914l-1.286-1.286C17.644 14.536 18 13.19 18 12c0-1.771-.775-3.9-2-5v7.586l-2-2V4C14 3.631 13.797 3.292 13.472 3.118zM12 5.868v4.718L9.169 7.755 12 5.868zM3 17h3.697l5.748 3.832C12.612 20.943 12.806 21 13 21c.162 0 .324-.039.472-.118C13.797 20.708 14 20.369 14 20v-1.879l-2-2v2.011l-4.445-2.964C7.391 15.059 7.197 15 7 15H4V9h.879L2.899 7.021C2.396 7.073 2 7.483 2 8v8C2 16.553 2.447 17 3 17z'/></svg>",
-  unmute: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M3 17h3.697l5.748 3.832a1.004 1.004 0 0 0 1.027.05A1 1 0 0 0 14 20V4a1 1 0 0 0-1.554-.832L6.697 7H3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1zm1-8h3c.197 0 .391-.059.555-.168L12 5.868v12.264l-4.445-2.964A1.006 1.006 0 0 0 7 15H4V9zM16 21c3.527-1.547 5.999-4.909 5.999-9S19.527 4.547 16 3v2c2.387 1.386 3.999 4.047 3.999 7S18.387 17.614 16 19v2z'/><path d='M16 7v10c1.225-1.1 2-3.229 2-5s-.775-3.9-2-5z'/></svg>"
+  unmute: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M3 17h3.697l5.748 3.832a1.004 1.004 0 0 0 1.027.05A1 1 0 0 0 14 20V4a1 1 0 0 0-1.554-.832L6.697 7H3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1zm1-8h3c.197 0 .391-.059.555-.168L12 5.868v12.264l-4.445-2.964A1.006 1.006 0 0 0 7 15H4V9zM16 21c3.527-1.547 5.999-4.909 5.999-9S19.527 4.547 16 3v2c2.387 1.386 3.999 4.047 3.999 7S18.387 17.614 16 19v2z'/><path d='M16 7v10c1.225-1.1 2-3.229 2-5s-.775-3.9-2-5z'/></svg>",
+  fullscreen: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M5 5h5V3H4a1 1 0 0 0-1 1v6h2V5zM4 21h6v-2H5v-5H3v6a1 1 0 0 0 1 1zM21 20v-6h-2v5h-5v2h6a1 1 0 0 0 1-1zM14 5h5v5h2V4a1 1 0 0 0-1-1h-6v2z'/></svg>",
+  exitfullscreen: "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><path d='M10 9V3H8v5H3v2h6a1 1 0 0 0 1-1zM8 21h2v-6a1 1 0 0 0-1-1H3v2h5v5zM21 14h-6a1 1 0 0 0-1 1v6h2v-5h5v-2zM21 8h-5V3h-2v6a1 1 0 0 0 1 1h6V8z'/></svg>"
 };
+
+let activeFullScreen = false;
 
 
 btnPlay.addEventListener('click', playState);
@@ -31,6 +35,7 @@ volumeControl.addEventListener('input', volumeState);
 (function(){
   btnPlay.innerHTML = icons.play;
   btnMute.innerHTML = icons.unmute;
+  btnFullscreen.innerHTML = icons.fullscreen;
 })();
 
 
@@ -116,14 +121,14 @@ function muteState(){
 }
 
 function fullscreenState() {
-  let video = videoPlayerMedia;
-
-  if(video.requestFullscreen){
-    video.requestFullscreen();
-  }else if(video.mozRequestFullScreen){
-    video.mozRequestFullScreen();
-  }else if(video.webkitRequestFullScreen){
-    video.webkitRequestFullScreen();
+  if(!activeFullScreen){
+    videoPlayer.requestFullscreen();
+    activeFullScreen = true;
+    btnFullscreen.innerHTML = icons.exitfullscreen;
+  }else{
+    document.exitFullscreen();
+    activeFullScreen = false;
+    btnFullscreen.innerHTML = icons.fullscreen;
   }
 }
 
@@ -166,8 +171,5 @@ function getTimeTooltip(e){
 }
 
 function volumeState(e){
-  let video = videoPlayerMedia,
-      volume = e.target.value;
-
-  video.volume = volume;
+  videoPlayerMedia.volume = e.target.value;
 }
